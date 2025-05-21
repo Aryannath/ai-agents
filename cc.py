a2ebf20f59a3c129bfbb8bbdacf4b8a4c779dc29
Aryannath <aryannath2@gmail.com>
2025-05-21 22:40:45 +0530

    added new fixes

---
+++ cc.py
@@ -0,0 +1,10 @@
+def two_sum(nums, target):
+    num_dict = {}
+    for i, num in enumerate(nums):
+        if target - num in num_dict:
+            return [num_dict[target - num], i]
+        num_dict[num] = i
+    return None
+
+def multiply(a, b): 
+    return a * b
